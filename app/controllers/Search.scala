3c5fc7f63f01e412a9b8e88975a53c9f97ef842e
Martin Knoller Stocker <martin.knoller@gmail.com>
2014-01-29 22:47:56 +0100

    loose the cache for the search

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -23,22 +23,12 @@ object Search extends Controller with SearchForm {
     boundedForm.fold(
     error =>  BadRequest,
     text => {
-      val shows =Cache.getAs[List[TVShow]](titleKeys)
-      val list = shows match {
-        case None =>{
-          val list= Media.allTitlesWithCount
-          Cache.set(titleKeys, list)
-          list
-        }
-        case Some(list) => list
-      }
-
-      val searchResult = list  filter{ case TVShow(title,_) => title.toLowerCase.contains(text.text.toLowerCase) }
+      val searchResult = Media.allTitlesWithCount  filter{ case TVShow(title,_) => title.toLowerCase.contains(text.text.toLowerCase) }
 
       // different result pages for zero, one or n results
       searchResult match {
         case Nil => Ok(views.html.searchNoResult(text.text))
-        case res :: Nil => Ok(views.html.show(Media.show(res.title), 0, res.title))
+        case singleResult :: Nil => Ok(views.html.show(Media.show(singleResult.title), 0, singleResult.title))
         case _ => Ok(views.html.searchresult(searchResult))
       }
 
